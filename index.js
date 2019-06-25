93410afe6ad9ead6e738b40524a3405f59bb9ef9
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-06-25 12:34:06 -0700

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+
 var app = express();
+var upload = multer({dest: './uploads'});
+
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.post('/', upload.single('myFile'), function(req, res){
+  cloudinary.uploader.upload(req.file.path, function(result){
+    console.log(result);
+    var imgUrl = cloudinary.url(result.public_id);
+    res.render('index', {url: imgUrl});
+  })
 });
 
 app.listen(3000);
